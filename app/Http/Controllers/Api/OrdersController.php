2a0399f1ca449be7f24eed90d10585e4e01566f8
apoliten <aponkinos@gmail.com>
2024-11-05 16:21:09 +0200

    update

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -227,7 +227,7 @@ class OrdersController extends Controller
 
     public function updateOrderStatus(Order $order, Request $request): JsonResponse|Response
     {
-        if($order->status == 'Завершено' && auth()->id() !== 'fd5ad647-cf2a-4add-9085-264072a63d4e' or  auth()->id() !== 'bcebb91f-901c-4997-af50-1967a0d122d6' )
+        if($order->status == 'Завершено' && auth()->id() == '7560883a-6f58-408d-919d-b7843dfd76b9' )
             return \response()->json([],400);
         $order->status = ($request->status == 'Взято в роботу' ? 'Завершено' : 'Взято в роботу');
         $order->finish_work = ($request->status == 'Взято в роботу' ? Carbon::now() : null) ;
