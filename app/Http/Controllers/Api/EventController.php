07711f9013cc881ff4f53b44ef4a82d5e5456bed
apoliten <aponkinos@gmail.com>
2025-03-03 22:46:13 +0200

    upd

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\CRM\Brand;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(auth()->id());
+
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->name,
+            'role' => $user->roles[0]['name'] ?? null
+        ]);
+    }
+}
